00df6693f2b9ae536a0cac1f4e1da989aea1b56c
Achraf Meziouni <amezioun@student.1337.ma>
2026-05-31 23:34:52 +0100

    remove otplib adapter esm issue

--- services/auth/src/controllers/twoFA.controller.ts
+++ services/auth/src/controllers/twoFA.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
-import { authenticator } from '@otplib/v12-adapter';
+// import { authenticator } from '@otplib/v12-adapter';
+import { authenticator } from 'otplib';
 import QRCode from 'qrcode';
 import prisma from '../lib/prisma';
 import { generateAccessToken, generateRefreshToken, verifyTempToken } from '../lib/token';
